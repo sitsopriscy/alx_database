a5e572fa1181bce18ee0543c60e24787611e2b05
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-14 14:19:10 -0800

    States table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,3 +2,9 @@
 
 -- creates a new database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+
+-- creates a table states (in the database hbtn_0d_usa) 
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
+
