a120c9c39944e0a39a7dbac8c7e4eaad91fed648
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-14 12:48:30 -0800

    First add

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
--- creates the database hbtn_0c_0 if it does not exist --
+-- creates the database hbtn_0c_0 if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
 
--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
--- deletes the database hbtn_0c_0 if it exist --
+-- deletes the database hbtn_0c_0 if it exist
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
 
