cbc0cbeb7c3672d8f0bee8cc892068b5e7f855a8
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-14 13:42:26 -0800

    Read user 1

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -6,3 +6,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates user user_0d_2 with password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
+
+-- grants select privileges to user_0d_2 on database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
