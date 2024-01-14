7c10c3068266e7a88f9c2066d1a4c0e818a61e76
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-14 13:40:29 -0800

    Read user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- a script that creates the database hbtn_0d_2 and the user user_0d_2
+
+-- creates a database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- creates user user_0d_2 with password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
