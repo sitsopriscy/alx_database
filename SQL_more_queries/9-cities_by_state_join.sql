55c760658ce6489f40b01adc9ddf0f9ce12003bb
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-16 08:56:50 -0800

    Cities by States 1

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa
 
-SELECT cities.id, cities.name, states.name as state_name
+SELECT cities.id, cities.name, states.name AS state_name
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id;
