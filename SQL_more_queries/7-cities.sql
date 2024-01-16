1f10ced4f04984de98cf48a601a7996d21a98dbd
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-16 08:45:36 -0800

    Cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,3 +1,11 @@
--- a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+-- a script that creates the database 'hbtn_0d_usa' and the table cities (in the database 'hbtn_0d_usa')
 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- creates a table 'states' (in the database 'hbtn_0d_usa') 
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY, 
+    state_id INT NOT NULL, name VARCHAR(256) NOT NULL, 
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+);
